76d1e12f463cb8dec8bb643911eca341a77f5252
kkawai <kkawai2@gmail.com>
2018-10-11 03:31:49 -0700

    Fixed play services ad version

--- app/src/main/java/net/atlasianvdopia/MainActivity.java
+++ app/src/main/java/net/atlasianvdopia/MainActivity.java
@@ -28,9 +28,9 @@ public class MainActivity extends AppCompatActivity implements RewardedAdListene
     private final static String CHOCOLATE_APP_KEY = "XqjhRR";
     private final static String APPODEAL_APP_KEY = "ccbd57eeec72f3baa5a081bc26d8e1ad9b7bbac0f1a4c273";
 
-    LVDOAdRequest adRequest = new LVDOAdRequest(this);
-    LVDOInterstitialAd interstitialAd;
-    LVDORewardedAd rewardedAd;
+    private LVDOAdRequest adRequest = new LVDOAdRequest(this);
+    private LVDOInterstitialAd interstitialAd;
+    private LVDORewardedAd rewardedAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,18 +39,22 @@ public class MainActivity extends AppCompatActivity implements RewardedAdListene
         adRequest.addPartnerName(LVDOConstants.PARTNER.ALL);
         Appodeal.disableLocationPermissionCheck();
         Appodeal.initialize(this, APPODEAL_APP_KEY, Appodeal.INTERSTITIAL | Appodeal.REWARDED_VIDEO | Appodeal.MREC);
-        Chocolate.init(this, CHOCOLATE_APP_KEY, new InitCallback() {
-            @Override
-            public void onSuccess() {
-                Log.d(TAG, "Chocolate.init success");
-                LVDORewardedAd.prefetch(MainActivity.this, CHOCOLATE_APP_KEY, adRequest);
-            }
+        if (!Chocolate.isInitialized(this)) {
+            Chocolate.init(this, CHOCOLATE_APP_KEY, new InitCallback() {
+                @Override
+                public void onSuccess() {
+                    Log.d(TAG, "Chocolate.init success");
+                    LVDORewardedAd.prefetch(MainActivity.this, CHOCOLATE_APP_KEY, adRequest);
+                }
 
-            @Override
-            public void onError(String s) {
-                Log.d(TAG, "Chocolate.init error: " + s);
-            }
-        });
+                @Override
+                public void onError(String s) {
+                    Log.d(TAG, "Chocolate.init error: " + s);
+                }
+            });
+        } else {
+            Log.d(TAG, "Chocolate is already initialized");
+        }
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -58,54 +62,54 @@ public class MainActivity extends AppCompatActivity implements RewardedAdListene
         Appodeal.setInterstitialCallbacks(new InterstitialCallbacks() {
             @Override
             public void onInterstitialLoaded(boolean isPrecache) {
-                Log.d(TAG, "onInterstitialLoaded");
+                Log.d(TAG, "Appodeal onInterstitialLoaded");
             }
 
             @Override
             public void onInterstitialFailedToLoad() {
-                Log.d(TAG, "onInterstitialFailedToLoad");
+                Log.d(TAG, "Appodeal onInterstitialFailedToLoad");
             }
 
             @Override
             public void onInterstitialShown() {
-                Log.d(TAG, "onInterstitialShown");
+                Log.d(TAG, "Appodeal onInterstitialShown");
             }
 
             @Override
             public void onInterstitialClicked() {
-                Log.d(TAG, "onInterstitialClicked");
+                Log.d(TAG, "Appodeal onInterstitialClicked");
             }
 
             @Override
             public void onInterstitialClosed() {
-                Log.d(TAG, "onInterstitialClosed");
+                Log.d(TAG, "Appodeal onInterstitialClosed");
             }
         });
 
         Appodeal.setRewardedVideoCallbacks(new RewardedVideoCallbacks() {
             @Override
             public void onRewardedVideoLoaded() {
-                Log.d(TAG, "onRewardedVideoLoaded");
+                Log.d(TAG, "Appodeal onRewardedVideoLoaded");
             }
 
             @Override
             public void onRewardedVideoFailedToLoad() {
-                Log.d(TAG, "onRewardedVideoFailedToLoad");
+                Log.d(TAG, "Appodeal onRewardedVideoFailedToLoad");
             }
 
             @Override
             public void onRewardedVideoShown() {
-                Log.d(TAG, "onRewardedVideoShown");
+                Log.d(TAG, "Appodeal onRewardedVideoShown");
             }
 
             @Override
             public void onRewardedVideoFinished(int amount, String name) {
-                Log.d(TAG, "onRewardedVideoFinished");
+                Log.d(TAG, "Appodeal onRewardedVideoFinished");
             }
 
             @Override
             public void onRewardedVideoClosed(boolean finished) {
-                Log.d(TAG, "onRewardedVideoClosed");
+                Log.d(TAG, "Appodeal onRewardedVideoClosed");
             }
         });
 
@@ -170,72 +174,72 @@ public class MainActivity extends AppCompatActivity implements RewardedAdListene
 
     @Override
     public void onRewardedVideoLoaded(LVDORewardedAd lvdoRewardedAd) {
-        Log.d(TAG, "onRewardedVideoLoaded");
+        Log.d(TAG, "chocolate onRewardedVideoLoaded");
         try {
             lvdoRewardedAd.showRewardAd("secret234", "myUserId", "coins", "4.00");
         } catch (Exception e) {
-            Log.e(TAG, "onRewardedVideoLoaded");
+            Log.e(TAG, "chocolate  onRewardedVideoLoaded: "+e);
         }
     }
 
     @Override
     public void onRewardedVideoFailed(LVDORewardedAd lvdoRewardedAd, LVDOConstants.LVDOErrorCode lvdoErrorCode) {
-        Log.d(TAG, "onRewardedVideoFailed");
+        Log.d(TAG, "chocolate onRewardedVideoFailed");
         toast("Chocolate No-Fill for rewarded");
     }
 
     @Override
     public void onRewardedVideoShown(LVDORewardedAd lvdoRewardedAd) {
-        Log.d(TAG, "onRewardedVideoShown");
+        Log.d(TAG, "chocolate onRewardedVideoShown");
     }
 
     @Override
     public void onRewardedVideoShownError(LVDORewardedAd lvdoRewardedAd, LVDOConstants.LVDOErrorCode lvdoErrorCode) {
-        Log.d(TAG, "onRewardedVideoShownError");
+        Log.d(TAG, "chocolate onRewardedVideoShownError");
     }
 
     @Override
     public void onRewardedVideoDismissed(LVDORewardedAd lvdoRewardedAd) {
-        Log.d(TAG, "onRewardedVideoFinished");
-        toast("Chocolate Dismissed");
+        Log.d(TAG, "chocolate onRewardedVideoFinished: "+lvdoRewardedAd.getWinningPartnerName());
+        toast("Chocolate Dismissed: " + lvdoRewardedAd.getWinningPartnerName());
         LVDORewardedAd.prefetch(MainActivity.this, CHOCOLATE_APP_KEY, adRequest);
     }
 
     @Override
     public void onRewardedVideoCompleted(LVDORewardedAd lvdoRewardedAd) {
-        Log.d(TAG, "onRewardedVideoCompleted");
+        Log.d(TAG, "chocolate onRewardedVideoCompleted");
     }
 
     @Override
     public void onInterstitialLoaded(LVDOInterstitialAd lvdoInterstitialAd) {
-        Log.d(TAG, "onInterstitialLoaded");
+        Log.d(TAG, "chocolate onInterstitialLoaded");
         try {
             interstitialAd.show();
         } catch (Exception e) {
-            Log.e(TAG, "onInterstitialLoaded: " + e);
+            Log.e(TAG, "chocolate onInterstitialLoaded: " + e);
         }
     }
 
     @Override
     public void onInterstitialFailed(LVDOInterstitialAd lvdoInterstitialAd, LVDOConstants.LVDOErrorCode lvdoErrorCode) {
-        Log.d(TAG, "onInterstitialFailed");
+        Log.d(TAG, "chocolate onInterstitialFailed");
         toast("Chocolate No-Fill for Interstitial");
     }
 
     @Override
     public void onInterstitialShown(LVDOInterstitialAd lvdoInterstitialAd) {
-        Log.d(TAG, "onInterstitialShown");
+        Log.d(TAG, "chocolate onInterstitialShown");
     }
 
     @Override
     public void onInterstitialClicked(LVDOInterstitialAd lvdoInterstitialAd) {
-        Log.d(TAG, "onInterstitialClicked");
+        Log.d(TAG, "chocolate chocolate onInterstitialClicked");
     }
 
     @Override
     public void onInterstitialDismissed(LVDOInterstitialAd lvdoInterstitialAd) {
-        Log.d(TAG, "onInterstitialDismissed");
-        toast("Chocolate Dismissed");
+        Log.d(TAG, "chocolate onInterstitialDismissed");
+        toast("Chocolate Dismissed: " + lvdoInterstitialAd.getWinningPartnerName());
         LVDOInterstitialAd.prefetch(MainActivity.this, CHOCOLATE_APP_KEY, adRequest);
     }
 
